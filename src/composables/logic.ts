aac00f62e5e80fa626c85ae2f1db2ae737b18365
fanfan <1075456035@qq.com>
2022-09-24 17:22:15 +0800

    chore: update

--- src/composables/logic.ts
+++ src/composables/logic.ts
@@ -1,3 +1,4 @@
+import type { Ref } from 'vue'
 import type { BlockState } from '~/types'
 
 const directions = [
@@ -11,9 +12,13 @@ const directions = [
   [0, 1],
 ]
 
+interface GameState {
+  board: BlockState[][]
+  mineGenerated: boolean
+  gameState: 'play' | 'won' | 'lost'
+}
 export class GamePlay {
-  state = ref<BlockState[][]>([])
-  mineGenerated = ref(false)
+  state = ref() as Ref<GameState>
 
   constructor(
     public width: number,
@@ -22,18 +27,25 @@ export class GamePlay {
     this.reset()
   }
 
+  get board() {
+    return this.state.value?.board
+  }
+
   reset() {
-    this.mineGenerated.value = false
-    this.state.value = Array.from({ length: this.height }, (_, y) =>
-      Array.from({ length: this.width },
-        (_, x): BlockState => ({
-          x,
-          y,
-          adjacentMines: 0,
-          revealed: false,
-        }),
+    this.state.value = {
+      mineGenerated: false,
+      gameState: 'play',
+      board: Array.from({ length: this.height }, (_, y) =>
+        Array.from({ length: this.width },
+          (_, x): BlockState => ({
+            x,
+            y,
+            adjacentMines: 0,
+            revealed: false,
+          }),
+        ),
       ),
-    )
+    }
   }
 
   generateMines(state: BlockState[][], initial: BlockState) {
@@ -50,7 +62,7 @@ export class GamePlay {
   }
 
   updateNumbers() {
-    this.state.value.forEach((raw) => {
+    this.board.forEach((raw) => {
       raw.forEach((block) => {
         if (block.mine)
           return
@@ -83,15 +95,19 @@ export class GamePlay {
   }
 
   onClick(block: BlockState) {
-    if (!this.mineGenerated.value) {
-      this.generateMines(this.state.value, block)
-      this.mineGenerated.value = true
-      console.log(true)
+    if (this.state.value.gameState !== 'play')
+      return
+
+    if (!this.state.value.mineGenerated) {
+      this.generateMines(this.board, block)
+      this.state.value.mineGenerated = true
     }
 
     block.revealed = true
-    if (block.mine)
+    if (block.mine) {
+      this.state.value.gameState = 'lost'
       alert('BOOOOM!')
+    }
     this.expendZero(block)
   }
 
@@ -101,20 +117,24 @@ export class GamePlay {
       const y2 = block.y + dy
       if (x2 < 0 || x2 >= this.width || y2 < 0 || y2 >= this.height)
         return undefined
-      return this.state.value[y2][x2]
+      return this.board[y2][x2]
     })
       .filter(Boolean) as BlockState[]
   }
 
   checkGameState() {
-    if (!this.mineGenerated.values)
+    if (!this.state.value.mineGenerated)
       return
-    const blocks = this.state.value.flat()
+    const blocks = this.board.flat()
     if (blocks.every(block => block.revealed || block.flagged)) {
-      if (blocks.some(block => block.flagged && !block.mine))
+      if (blocks.some(block => block.flagged && !block.mine)) {
+        this.state.value.gameState = 'lost'
         alert('You cheat!')
-      else
+      }
+      else {
+        this.state.value.gameState = 'won'
         alert('You win!')
+      }
     }
   }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,34 +4,40 @@
 import MineBlock from '../components/MineBlock.vue'
 import { GamePlay, isDev, toggleDev } from '~/composables'
 const play = new GamePlay(5, 5)
-const state = play.state
+const state = computed(() => play.board)
+useStorage('vue-minesweeper', play.state)
 watchEffect(() => {
   play.checkGameState()
+})
+watch(state, () => {
+  console.log('watch state')
 })
 </script>
 
 <template>
   <div>
     Minesweeper
-    <div
-      v-for="row, y in state"
-      :key="y"
-      flex="~"
-      items-center justify-center
-    >
-      <MineBlock
-        v-for="block, x in row"
-        :key="x"
-        :block="block"
-        @click="play.onClick(block)"
-        @contextmenu.prevent="play.onRightClick(block)"
-      />
+    <div p5>
+      <div
+        v-for="row, y in state"
+        :key="y"
+        flex="~"
+        items-center justify-center
+      >
+        <MineBlock
+          v-for="block, x in row"
+          :key="x"
+          :block="block"
+          @click="play.onClick(block)"
+          @contextmenu.prevent="play.onRightClick(block)"
+        />
+      </div>
     </div>
     <div flex="~ gap-1" justify-center>
       <button btn @click="toggleDev()">
         {{ isDev ? "DEV" : "NORMAL" }}
       </button>
-      <button btn @click="play.reset">
+      <button btn @click="play.reset()">
         RESET
       </button>
     </div>
